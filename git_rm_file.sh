4286adce560a2fff9da9ab4214c30eb875e10d93
hachisu_ryuma <hachisu_ryuma@grenge.co.jp>
2014-07-24 11:44:23 +0900

    shell追加

--- git_rm_file.sh
+++ git_rm_file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for i in `git status | tail -n +5 | awk '{ print $2 }'`; do rm -rf $i; done;for i in `git status | tail -n +5 | awk '{ print $2 }'`; do rm -rf $i; done;
+for i in `git status | tail -n +7 | awk '{ print $1 }'`; do rm -rf $i; done;
