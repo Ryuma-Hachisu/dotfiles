fa7df7cbcb32e04965b026e69c7ea1e0e374beda
Ryuma-Hachisu <eagle19860122@gmail.com>
2014-09-27 15:00:34 +0900

    comment 機能追加

--- git_setting_ios.sh
+++ git_setting_ios.sh
@@ -9,3 +9,8 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 # updatedb使えるようにする
 alias updatedb='/usr/libexec/locate.updatedb'
 git config --global color.ui auto
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist 'log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short'
